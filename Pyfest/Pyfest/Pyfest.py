ddb3e45e814e13ee9b5799cd47029319d53f18c0
goofy2 <lldef968@learner.dvusd.org>
2026-01-12 13:57:35 -0700

    first version

--- Pyfest/Pyfest/Pyfest.py
+++ Pyfest/Pyfest/Pyfest.py
@@ -1,1 +1,22 @@
+print("---py-fest 2026 Stage Manager---")
+print("1. view Lineup & total time")
+print("2. Add a New Band ")
+print("3 Move First Band to End (Late Arrival) ")
+print("4. Remove a Band By Name")
+print("5. Move Band to Specific Position")
+print("6. Exit")
+choice = input("select an option (1-6): ")
+
+
+
+
+#parts
+if choice == "1":
+
+elif choice == "2":
+elif choice == "3":
+elif choice == "4":
+elif choice == "5":
+elif choice == "6":
+    break
 
